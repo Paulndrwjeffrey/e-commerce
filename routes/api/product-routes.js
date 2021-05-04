193f0045213ee77fb1a612e247c61e338263f44c
pablo <paulandrewjeffrey@gmail.com>
2021-05-03 17:43:51 -0700

    almost working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,47 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
-// get all products
+// find all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+    Product.findAll({
+      include: [
+        {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]})
+    .then(data => res.json(data))
+    .catch(error => {
+      console.log(error);
+      res.status(500).json(error);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+      model: Category,
+      attributes: ['category_name']
+    },
+    {
+      model: Tag,
+      attributes: ['tag_name']
+    }
+  ]})
+  .then(data => res.json(data))
+  .catch(error => {
+    console.log(error);
+    res.status(500).json(error);
+  });
 });
 
 // create new product
@@ -90,7 +119,14 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(data => res.json(json)).catch(error => {
+    console.log(error);
+    res.status(500).json(error);
+  })
 });
 
 module.exports = router;
